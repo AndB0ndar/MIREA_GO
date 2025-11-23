d2466ec1a5442160b1b303dfb6afdc2ee37e9a2c
AndB0ndar <andrey.bondar.2003 at list dot ru>
2025-11-23 21:30:56 +0300

    12. Complite

--- 12/app/cmd/server/main.go
+++ 12/app/cmd/server/main.go
@@ -9,51 +9,51 @@
 package main
 
 import (
-    "log"
-    "net"
-    "net/http"
-    "os"
+	"log"
+	"net"
+	"net/http"
+	"os"
 
-    httpSwagger "github.com/swaggo/http-swagger"
+	httpSwagger "github.com/swaggo/http-swagger"
 
-    httpx "app/internal/http"
-    "app/internal/http/handlers"
-    "app/internal/platform/config"
-    "app/internal/repo"
+	httpx "app/internal/http"
+	"app/internal/http/handlers"
+	"app/internal/platform/config"
+	"app/internal/repo"
 )
 
 // @host arbond.ru
 // @schemes http
 func main() {
-    cfg := config.Load()
+	cfg := config.Load()
 
-    if _, err := os.Stat(cfg.SocketPath); err == nil {
-        log.Fatal("Error: socket file already exists at ", cfg.SocketPath)
-    }
-    listener, err := net.Listen("unix", cfg.SocketPath)
-    if err != nil {
-        log.Fatal("Failed to create Unix socket listener:", err)
-    }
-    defer func() {
-        listener.Close()
-        os.Remove(cfg.SocketPath)
-    }()
-    if err := os.Chmod(cfg.SocketPath, 0666); err != nil {
-        log.Fatal("Failed to set socket permissions:", err)
-    }
+	if _, err := os.Stat(cfg.SocketPath); err == nil {
+		log.Fatal("Error: socket file already exists at ", cfg.SocketPath)
+	}
+	listener, err := net.Listen("unix", cfg.SocketPath)
+	if err != nil {
+		log.Fatal("Failed to create Unix socket listener:", err)
+	}
+	defer func() {
+		listener.Close()
+		os.Remove(cfg.SocketPath)
+	}()
+	if err := os.Chmod(cfg.SocketPath, 0666); err != nil {
+		log.Fatal("Failed to set socket permissions:", err)
+	}
 
-    noteRepo := repo.NewNoteRepoMem()
-    handler := &handlers.Handler{Repo: noteRepo}
-    router := httpx.NewRouter(handler)
+	noteRepo := repo.NewNoteRepoMem()
+	handler := &handlers.Handler{Repo: noteRepo}
+	router := httpx.NewRouter(handler)
 
-    // router.Get("/docs/*", httpSwagger.WrapHandler)
-    router.Get("/docs/*", httpSwagger.Handler(
-        httpSwagger.URL("swagger.json"),
-    ))
-    router.Get("/docs/swagger.json", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "docs/swagger.json")
-    })
+	// router.Get("/docs/*", httpSwagger.WrapHandler)
+	router.Get("/docs/*", httpSwagger.Handler(
+		httpSwagger.URL("swagger.json"),
+	))
+	router.Get("/docs/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "docs/swagger.json")
+	})
 
-    log.Println("Server starting on Unix socket:", cfg.SocketPath)
-    log.Fatal(http.Serve(listener, router))
+	log.Println("Server starting on Unix socket:", cfg.SocketPath)
+	log.Fatal(http.Serve(listener, router))
 }
